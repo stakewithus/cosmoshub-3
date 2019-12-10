1012b93da82b4ab7dbe315cde209e3c883be1835
cryptobabybear <hello@oliverwee.com>
2019-12-10 09:25:13 +0000

    Updated Migrate

--- migrate.py
+++ migrate.py
@@ -21,7 +21,50 @@ export \
 > exported.json
 """
     try:
-        export_output = subprocess.check_output(docker_cmd, shell=True, stderr=None)
+        export_output = subprocess.check_output(docker_cmd, shell=True)
+        return export_output
+    except subprocess.CalledProcessError as err:
+        print(err)
+
+
+def checksum_exported(settings):
+    source_dir = settings["default"]["SourceDirectory"]
+    arrange_proc = subprocess.Popen(
+        "jq -S -c -M '' exported.json",
+        shell=True,
+        cwd=source_dir,
+        stdout=subprocess.PIPE,
+    )
+    output = subprocess.check_output(
+        ("shasum", "-a", "256"),
+        stdin=arrange_proc.stdout,
+    )
+    arrange_proc.wait()
+    checksum = str(output, "utf-8")
+    print(f"Verify SHASUM: {checksum}")
+
+
+def migrate_exported(settings):
+    source_dir = settings["default"]["SourceDirectory"]
+    if settings["migration"]["SourceIncludesNetwork"] == "yes":
+        source_dir += "/" + settings["migration"]["FromNetwork"]
+    to_image = settings["migration"]["ToImage"]
+    to_network = settings["migration"]["ToNetwork"]
+
+    docker_cmd = f"""docker run \
+--mount type=bind,src={source_dir},dst=/opt/chain \
+-w /opt/chain \
+--rm -it {to_image} \
+migrate \
+v0.36 \
+exported.json \
+--chain-id={to_network} \
+--genesis-time=2019-09-24T13:38:19Z \
+> {to_network}-genesis.json
+"""
+    try:
+        export_output = subprocess.check_output(docker_cmd, shell=True)
+        return export_output
     except subprocess.CalledProcessError as err:
         print(err)
 
@@ -53,7 +96,11 @@ def main():
     }
 
     # Export For Height
-    export_for_height(settings)
+    # export_for_height(settings)
+    # Check SHA256 SUM
+    checksum_exported(settings)
+
+    migrate_exported(settings)
 
 
 if __name__ == "__main__":
